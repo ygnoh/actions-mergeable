6b998c89b05cb03c43bfe16a370978993a52d2c3
Yonggoo Noh <yonggoo.noh@gmail.com>
2022-07-04 13:55:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const core = require("@actions/core");
 console.log(core.getInput("prNumber"));
 console.log(core.getInput("pr"));
 console.log(process.env.GITHUB_API_URL);
-console.log(process.env.GITHUB_ACTION_REPOSITORY);
-console.log(process.env.GITHUB_BASE_REF);
 console.log(process.env.GITHUB_HEAD_REF);
 console.log(process.env.GITHUB_REF_NAME);
 console.log(process.env.GITHUB_REPOSITORY);
